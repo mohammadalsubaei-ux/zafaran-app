43dadec6ab6107fe468b711f81a69f6307d3a8aa
Mohammad <algamar2010@hotmail.com>
2026-05-11 17:07:33 +0300

    add LanguageProvider to root

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,25 +3,28 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 import { CartProvider } from '@/context/CartContext';
+import { LanguageProvider } from '@/context/LanguageContext';
 
 export default function RootLayout() {
   return (
     <ThemeProvider value={DarkTheme}>
-      <CartProvider>
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="login" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="chef/[id]" options={{ headerShown: false }} />
-          <Stack.Screen name="item/[id]" options={{ headerShown: false }} />
-          <Stack.Screen name="cart" options={{ headerShown: false }} />
-          <Stack.Screen name="orders/index" options={{ headerShown: false }} />
-          <Stack.Screen name="dashboard/index" options={{ headerShown: false }} />
-          <Stack.Screen name="driver/index" options={{ headerShown: false }} />
-          <Stack.Screen name="wallet" options={{ headerShown: false }} />
-        </Stack>
-        <StatusBar style="light" />
-      </CartProvider>
+      <LanguageProvider>
+        <CartProvider>
+          <Stack>
+            <Stack.Screen name="index"           options={{ headerShown: false }} />
+            <Stack.Screen name="login"           options={{ headerShown: false }} />
+            <Stack.Screen name="(tabs)"          options={{ headerShown: false }} />
+            <Stack.Screen name="chef/[id]"       options={{ headerShown: false }} />
+            <Stack.Screen name="item/[id]"       options={{ headerShown: false }} />
+            <Stack.Screen name="cart"            options={{ headerShown: false }} />
+            <Stack.Screen name="orders/index"    options={{ headerShown: false }} />
+            <Stack.Screen name="dashboard/index" options={{ headerShown: false }} />
+            <Stack.Screen name="driver/index"    options={{ headerShown: false }} />
+            <Stack.Screen name="wallet"          options={{ headerShown: false }} />
+          </Stack>
+          <StatusBar style="light" />
+        </CartProvider>
+      </LanguageProvider>
     </ThemeProvider>
   );
 }
